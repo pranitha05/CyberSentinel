15e74266e9cdb5e5148ec1cd5197ebf74dda9d7b
rpranitha0512 <rpranitha0512@gmail.com>
2025-07-05 05:34:35 +0000

    fixed run.py

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # For Render: bind to 0.0.0.0 and use the PORT env variable
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=True)
